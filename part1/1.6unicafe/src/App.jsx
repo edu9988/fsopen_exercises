ccefdbeed4a9cf76e37223df9320f2d4a851445a
edu9988 <edu_9988@hotmail.com>
2023-12-24 18:25:49 -0300

    Add exercises 1.6 and 1.7

--- part1/1.6unicafe/src/App.jsx
+++ part1/1.6unicafe/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <p>{counter}</p>
+const Display = ({name, result}) => <p>{name} {result}</p>
 
 const App = () => {
     const [good, setGood] = useState(0)
@@ -13,14 +13,14 @@ const App = () => {
 
     return (
 	<>
-	    <h1>give geedback</h1>
+	    <h1>give feedback</h1>
 	    <button onClick={hGood}>good</button>
 	    <button onClick={hNeutral}>neutral</button>
 	    <button onClick={hBad}>bad</button>
 	    <h1>statistics</h1>
-	    <Display counter={good} />
-	    <Display counter={neutral} />
-	    <Display counter={bad} />
+	    <Display name="good" result={good} />
+	    <Display name="neutral" result={neutral} />
+	    <Display name="bad" result={bad} />
 	</>
     )
 }
--- part1/1.7unicafe/src/App.jsx
+++ part1/1.7unicafe/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <p>{counter}</p>
+const Display = ({name, result, unit}) => <p>{name} {result} {unit}</p>
 
 const App = () => {
     const [good, setGood] = useState(0)
@@ -13,14 +13,17 @@ const App = () => {
 
     return (
 	<>
-	    <h1>give geedback</h1>
+	    <h1>give feedback</h1>
 	    <button onClick={hGood}>good</button>
 	    <button onClick={hNeutral}>neutral</button>
 	    <button onClick={hBad}>bad</button>
 	    <h1>statistics</h1>
-	    <Display counter={good} />
-	    <Display counter={neutral} />
-	    <Display counter={bad} />
+	    <Display name="good" result={good} />
+	    <Display name="neutral" result={neutral} />
+	    <Display name="bad" result={bad} />
+	    <Display name="all" result={good+neutral+bad} />
+	    <Display name="average" result={(good-bad)/(good+neutral+bad)} />
+	    <Display name="positive" result={100*(good)/(good+neutral+bad)} unit="%"/>
 	</>
     )
 }
