ee7c02df4e69bd901d1672cb96d3162d7dabdacb
edu9988 <edu.jerm@gmail.com>
2025-01-20 16:17:44 -0300

    Do exercise 3.20

--- Phonebook_backend/model/person.js
+++ Phonebook_backend/model/person.js
@@ -12,11 +12,21 @@ mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
     name: {
-        type:String,
+        type: String,
         minLength: 3,
         required: true
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: str => {
+                return /^\d?\d{2}-\d{8}\d?$/.test(str)
+            },
+            message: props => `${props.value} is not a valid phone number! First field is 2 or 3 digits, second field is 8 or 9 digits, they are separated by '-'. Example: 012-12345678`
+        },
+        required: true
+    }
 })
 personSchema.set('toJSON',{
     versionKey:false,
