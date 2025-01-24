00fe2bef5c075c8b0b1ccf7714856aa0a2e60041
edu9988 <edu.jerm@gmail.com>
2025-01-24 12:48:57 -0300

    Do exercise 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -7,7 +7,29 @@ const totalLikes = (blogs) => {
   )
 }
 
+const favoriteBlog = (blogs) => {
+  let max = blogs.length === 0
+    ? 0
+    : blogs[0].likes
+
+  const favoriteIndex = blogs.reduce(
+    (maxIndex,item,i) => {
+      if( blogs[i].likes > blogs[maxIndex].likes ){
+        max = blogs[i].likes
+        return i
+      }
+      return maxIndex
+    },
+    0
+  )
+
+  return blogs.length === 0
+    ? undefined
+    : blogs[favoriteIndex]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
