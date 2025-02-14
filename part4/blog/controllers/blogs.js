8143741ac87aa87cb8dec56d8873dc6276ceae13
edu9988 <edu.jerm@gmail.com>
2025-02-14 18:49:16 -0300

    Do exercise 4.20

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -13,19 +13,12 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if( authorization && authorization.startsWith('Bearer ') )
-    return authorization.replace('Bearer ', '')
-  return null
-}
-
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   if( !('likes' in body) )
     body.likes = 0
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if(!decodedToken.id)
     return response.status(401).json({error: 'token invalid'})
 
