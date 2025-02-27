2c57447c397700254748376c7f22daa93fb91cc2
edu9988 <edu.jerm@gmail.com>
2025-02-27 10:47:28 -0300

    Do exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders blog correctly', async () => {
+describe('<Blog />', () => {
   const blog = {
     title: 'Bandit',
     author: 'Duhamel',
@@ -16,69 +16,100 @@ test('renders blog correctly', async () => {
     id: '798789798'
   }
 
-  const mockHandler = vi.fn()
-  const secondMockHandler = vi.fn()
+  test('Shows only title and author by default', async () => {
+    const mockHandler = vi.fn()
+    const secondMockHandler = vi.fn()
 
-  const { container } = render(
-    <Blog
-      blog={blog}
-      handleLike={mockHandler}
-      handleDeletion={secondMockHandler}
-      owner={true}
-    />
-  )
+    const { container } = render(
+      <Blog
+        blog={blog}
+        handleLike={mockHandler}
+        handleDeletion={secondMockHandler}
+        owner={true}
+      />
+    )
 
-  const element = screen.getByText(
-    'Bandit Duhamel', { exact: true }
-  )
-  expect(element).toBeDefined()
-  expect(element).not.toBeNull()
+    const element = screen.getByText(
+      'Bandit Duhamel', { exact: true }
+    )
+    expect(element).toBeDefined()
+    expect(element).not.toBeNull()
 
-  const element3 = screen.queryByText(
-    'netflix.com/12345'
-  )
-  expect(element3).toBeNull()
-  console.log(element3)
+    const element2 = screen.queryByText(
+      'netflix.com/12345'
+    )
+    expect(element2).toBeNull()
 
-  const element4 = screen.queryByText(
-    'likes 1'
-  )
-  expect(element4).toBeNull()
-  console.log(element4)
+    const element3 = screen.queryByText(
+      'likes 1'
+    )
+    expect(element3).toBeNull()
 
-/*
-  const element = screen.getByText('Component testing is done with react-testing-library')
-  expect(element).toBeDefined()
-/*
-*/
+  /*
+    const element = screen.getByText('Component testing is done with react-testing-library')
+    expect(element).toBeDefined()
+  /*
+  */
 
-  //screen.debug() //prints html to console
+    //screen.debug() //prints html to console
 
-/*
-  const div = container.querySelector('.note')
-  expect(div).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
-*/
+  /*
+    const div = container.querySelector('.note')
+    expect(div).toHaveTextContent(
+      'Component testing is done with react-testing-library'
+    )
+  */
 
-/*
-  const element = screen.getByText('Component testing is done with react-testing-library')
+  /*
+    const element = screen.getByText('Component testing is done with react-testing-library')
 
-  screen.debug(element)
-  expect(element).toBeDefined()
-*/
+    screen.debug(element)
+    expect(element).toBeDefined()
+  */
 
-/*
-  const mockHandler = vi.fn()
+  /*
+    const mockHandler = vi.fn()
 
-  render(
-    <Note note={note} toggleImportance={mockHandler} />
-  )
+    render(
+      <Note note={note} toggleImportance={mockHandler} />
+    )
 
-  const user = userEvent.setup()
-  const button = screen.getByText('make not important')
-  await user.click(button)
+    const user = userEvent.setup()
+    const button = screen.getByText('make not important')
+    await user.click(button)
 
-  expect(mockHandler.mock.calls).toHaveLength(1)
-*/
+    expect(mockHandler.mock.calls).toHaveLength(1)
+  */
+  })
+
+  test('likes and url are shown after clicking \'view\'', async () => {
+    const mockHandler = vi.fn()
+    const secondMockHandler = vi.fn()
+
+    const { container } = render(
+      <Blog
+        blog={blog}
+        handleLike={mockHandler}
+        handleDeletion={secondMockHandler}
+        owner={true}
+      />
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const element = screen.getByText(
+      'netflix.com/12345', { exact: false }
+    )
+    expect(element).toBeDefined()
+    expect(element).not.toBeNull()
+
+    const element2 = screen.getByText(
+      'likes 1', { exact: false }
+    )
+    expect(element2).toBeDefined()
+    expect(element2).not.toBeNull()
+
+  })
 })
